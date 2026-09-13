14b330e9762d09211dd66849bb51ae8b2f62c17a
Alton Kesselly <kessellyalton@outlook.com>
2026-09-13 23:44:48 +0000

    fix(backend): add django.contrib.postgres for Wagtail Postgres search

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     "django_filters",
 
     # Django
+    "django.contrib.postgres",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
